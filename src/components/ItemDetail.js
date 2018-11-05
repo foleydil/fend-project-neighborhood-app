dac623b00040f9416c1fbc4449c259a17e72b832
Dillon Foley <foleydil@gmail.com>
2018-11-04 21:23:08 -0500

    refactor: Convert ItemDetail to stateless functional component

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -2,23 +2,24 @@
 
 import React, { Component } from 'react';
 
-class ItemDetail extends Component {
-
-  render() {
-    return (
-      <div className='item-detail'>
-        <img className='location-photo' alt={this.props.location.categories[0].shortName + ' icon'} src={this.props.location.categories[0].icon.prefix + 'bg_64' + this.props.location.categories[0].icon.suffix}/>
-          <div className='item-info-container'>
-            <h2 className='item-name'>{this.props.location.name}</h2>
-            <p className='item-type'>{this.props.location.categories[0].shortName}</p>
-            <p className='item-address'>{this.props.location.location.address} {this.props.location.location.city}, {this.props.location.location.state}</p>
-          </div>
+function ItemDetail (props) {
+  return (
+    <div className='item-detail'>
+      <img
+        className='location-photo'
+        alt={props.location.categories[0].shortName + ' icon'}
+        src={props.location.categories[0].icon.prefix + 'bg_64' + props.location.categories[0].icon.suffix}
+      />
+      <div className='item-info-container'>
+        <h2 className='item-name'>{props.location.name}</h2>
+        <p className='item-type'>{props.location.categories[0].shortName}</p>
+        <p className='item-address'>{props.location.location.address} {props.location.location.city}, {props.location.location.state}</p>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
 export default ItemDetail;
 
-//Below snippet saved for item "locking" functionality, if I have time 
+//Below snippet saved for item "locking" functionality, to be implemented later
 //<img className='lock-toggle-icon' alt='unlocked icon' src={window.location.origin + '/res/002-unlock.png'}/>
