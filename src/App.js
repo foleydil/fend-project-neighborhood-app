0820edcfa6c02f81a9eccf2d0ad8f1b1dce3fb6b
Dillon Foley <foleydil@gmail.com>
2018-11-06 22:14:13 -0500

    refactor: Move map & marker instantiation to App.js

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+    this.loadMap();
     this.getFoursquareBusinesses();
   }
 
@@ -43,6 +44,71 @@ class App extends Component {
           console.log("Error: " + error)
         });
     }
+
+  loadMap = () => {
+    loadGoogleMapsScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyBe0pZu9OZtaR14XD_kcXjYwGQWyMfPKTg&callback=initMap');
+    window.initMap = this.initMap;
+  }
+
+  //create new map object based on given LatLong (currently set to ROYAL OAK MI)
+  initMap = () => {
+    window.map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 42.488598, lng: -83.144647},
+      zoom: 16
+    });
+  }
+
+  //method to open infowindow when marker is clicked
+  openInfoWindow(map, marker, infoWindow) {
+    return infoWindow.open(map, marker);
+  }
+
+  //update markers array to match displayedLocations
+  //passed as prop to Map.js
+  initMarkers = () => {
+    console.log(this.state.displayedLocations)
+    //TODO: CLEAR EXISTING MARKERS ON SEARCH
+    for (let l of this.state.displayedLocations) {
+      if (l.marker) {
+        l.marker.setVisible(false)
+      }
+    }
+
+    //infowindow object created outside loop to ensure only one is showing at a time
+    let newInfowindow = new window.google.maps.InfoWindow()
+
+    //generate markers for displayedLocations
+    for (let loc of this.state.displayedLocations) {
+      let marker = new window.google.maps.Marker({
+        position: {
+          lat: loc.location.lat,
+          lng: loc.location.lng
+        },
+        map: window.map,
+        title: loc.name
+      })
+
+      //content string for InfoWindow
+      let contentString =
+        `<div class='info-window'}>
+          <h3>${loc.name}</h3>
+          <img src='' alt=${loc.name} photo>
+          <ul class='info-window-address'>
+            <li>${loc.location.formattedAddress[0]}</li>
+            <li>${loc.location.formattedAddress[1]}</li>
+          </ul>
+        </div>`
+
+      //Listener for markers, updates & opens infowindow on click
+      marker.addListener('click', function() {
+        //update infoWindow content
+        newInfowindow.setContent(contentString)
+        newInfowindow.open(window.map, marker);
+      });
+
+      loc.marker = marker
+    }
+  }
 
   //Method to toggle whether list of locations is shown at bottom of screen.
   //passed a prop to DetailList.js
@@ -73,7 +139,7 @@ class App extends Component {
     return (
       <div className="App">
         <main>
-          <Map displayedLocations={this.state.displayedLocations}/>
+          <Map initMarkers={this.initMarkers}/>
           <DetailList
             toggleSearch={this.toggleSearch}
             updateSearch={this.updateSearch}
@@ -88,3 +154,14 @@ class App extends Component {
 }
 
 export default App;
+
+//Vanilla JS function to load google Maps script when app initially loads
+function loadGoogleMapsScript(url) {
+  let index = window.document.getElementsByTagName('script')[0];
+  let script = window.document.createElement('script');
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+
+  index.parentNode.insertBefore(script, index);
+}
--- src/components/Map.js
+++ src/components/Map.js
@@ -4,98 +4,16 @@ import React, { Component } from 'react';
 
 class Map extends Component {
 
-  //load map on initial load
-  componentDidMount() {
-    this.loadMap();
-  }
-
-  loadMap = () => {
-    loadGoogleMapsScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyBe0pZu9OZtaR14XD_kcXjYwGQWyMfPKTg&callback=initMap');
-    window.initMap = this.initMap;
-  }
-
-  //create new map object based on given LatLong (currently set to ROYAL OAK MI)
-  initMap = () => {
-    window.map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 42.488598, lng: -83.144647},
-      zoom: 16
-    });
-  }
-
-  //method to update displayed markers upon user search
+    //method to update displayed markers upon user search
   componentDidUpdate() {
-    console.log(this.props.displayedLocations);
-    this.initMarkers(window.map)
+    this.props.initMarkers();
   }
-
-  //method to open infowindow when marker is clicked
-  openInfoWindow(map, marker, infoWindow) {
-    return infoWindow.open(map, marker);
-  }
-
-  //Array of currently displayed markers
-  markers = [];
-
-  //update markers array to match displayedLocations
-  initMarkers = (map) => {
-    //clear existing markers
-    for (let m of this.markers) {
-      m.setMap(null);
-    }
-
-    //infowindow object created outside loop to ensure only one is showing at a time
-    let newInfowindow = new window.google.maps.InfoWindow()
-
-    //generate markers for displayedLocations
-    for (let loc of this.props.displayedLocations) {
-      let marker = new window.google.maps.Marker({
-        position: {
-          lat: loc.location.lat,
-          lng: loc.location.lng
-        },
-        map: map,
-        title: loc.name
-      })
-
-      //content string for InfoWindow
-      let contentString =
-        `<div class='info-window'}>
-          <h3>${loc.name}</h3>
-          <img src='' alt=${loc.name} photo>
-          <ul class='info-window-address'>
-            <li>${loc.location.formattedAddress[0]}</li>
-            <li>${loc.location.formattedAddress[1]}</li>
-          </ul>
-        </div>`
-
-      //Listener for markers, updates & opens infowindow on click
-      marker.addListener('click', function() {
-        //update infoWindow content
-        newInfowindow.setContent(contentString)
-        newInfowindow.open(map, marker);
-      });
-
-      this.markers.push(marker)
-    }
-  }
-
 
   render() {
     return (
         <div id='map'></div>
     );
   }
-}
-
-//Vanilla JS function to load google Maps script when app initially loads
-function loadGoogleMapsScript(url) {
-  let index = window.document.getElementsByTagName('script')[0];
-  let script = window.document.createElement('script');
-  script.src = url;
-  script.async = true;
-  script.defer = true;
-
-  index.parentNode.insertBefore(script, index);
 }
 
 export default Map;
